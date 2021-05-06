283a738e07716bd7e2be462f61e4d08636b64655
mflem8 <matthewfleming8@gmail.com>
2021-05-06 12:15:32 -0400

    update routes for testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :vinyls, only: [:index]
+      resources :vinyls, only: [:index, :create]
     end
   end
 end

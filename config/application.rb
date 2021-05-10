68447824156db53a64376c42c117cec7797acfab
mflem8 <matthewfleming8@gmail.com>
2021-05-10 13:39:59 -0400

    move to cors file

--- config/application.rb
+++ config/application.rb
@@ -21,15 +21,6 @@ Bundler.require(*Rails.groups)
 
 module VinylvaultBackend
   class Application < Rails::Application
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-          :max_age => 0
-      end
-    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
